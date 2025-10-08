82456254c2b0a637d9dd6f3451567fd2b064184d
danikCosiachniy <danilashumkevich@gmail.com>
2025-10-08 11:31:08 +0300

    added json and XML exporters

--- app/adapters/export_json.py
+++ app/adapters/export_json.py
@@ -1,7 +1,15 @@
 import json
 from app.ports.exporter import Exporter
 
+
 class JsonExporter(Exporter):
+    """
+    Реализация экспортера для сохранения данных в формате JSON.
+
+    Пример использования:
+        >>> data = {"rooms": [{"id": 1, "name": "Room #1"}]}
+        >>> JsonExporter().dump(data, "result.json")
+    """
     def dump(self, data: dict, path: str) -> None:
-        # json.dump(data, file, ensure_ascii=False, indent=2)
-        pass
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
--- app/adapters/export_xml.py
+++ app/adapters/export_xml.py
@@ -1,8 +1,25 @@
 import xml.etree.ElementTree as ET
 from app.ports.exporter import Exporter
 
+
 class XmlExporter(Exporter):
+    """
+    Реализация экспортера для сохранения данных в формате XML.
+
+    Пример:
+        >>> data = {"rooms": [{"id": 1, "name": "Room #1"}]}
+        >>> XmlExporter().dump(data, "result.xml")
+    """
+
     def dump(self, data: dict, path: str) -> None:
-        # root=<result>, для каждой секции <query name="..."><row>...</row></query>
-        # строковые значения, None -> ""
-        pass
+        root = ET.Element("result")
+
+        for section, rows in data.items():
+            query_elem = ET.SubElement(root, "query", name=section)
+            for row in rows:
+                row_elem = ET.SubElement(query_elem, "row")
+                for key, value in row.items():
+                    ET.SubElement(row_elem, key).text = str(value) if value is not None else ""
+
+        tree = ET.ElementTree(root)
+        tree.write(path, encoding="utf-8", xml_declaration=True)
